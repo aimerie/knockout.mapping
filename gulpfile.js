d4eabe5c5d3646191e5bc1e43333d8da201bad57
Kaite Aimerie <kaitebug@gmail.com>
2024-05-09 23:00:34 -0600

    es6+ updates, custom types

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,31 @@
 'use strict';
 /*jshint node:true*/
 
-var gulp = require('gulp');
-var del = require('del');
-var qunit = require('gulp-qunit');
-var plumber = require('gulp-plumber');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
-var replace = require('gulp-replace');
-var rename = require('gulp-rename');
-var header = require('gulp-header');
-var sourceMaps = require('gulp-sourcemaps');
+import gulp from "gulp";
+import { deleteAsync } from "del";
+import qunit from "gulp-qunit";
+import plumber from "gulp-plumber";
+import jshint from "gulp-jshint";
+import uglify from "gulp-uglify";
+import replace from "gulp-replace";
+import rename from "gulp-rename";
+import header from "gulp-header";
+import sourceMaps from 'gulp-sourcemaps';
+
+import _pkg from "./package.json" assert {type: "json"};
+
+var paths = {
+  output: ["dist/**/*.js", "dist/**/*.ts", "dist/**/*.map"]
+};
 
 var buildConfig = {
   outputPath: 'dist',
-  pkg: require('./package.json'),
+  pkg: _pkg,
   banner: [
     '/*!',
     ' * Knockout Mapping plugin v<%= pkg.version %>',
     ' * (c) 2013 Steven Sanderson, Roy Jacobs - http://knockoutjs.com/',
+    ' * Customizations by k80uu',
     ' * License: MIT (http://www.opensource.org/licenses/mit-license.php)',
     ' */\n'
   ].join('\n')
@@ -26,11 +33,14 @@ var buildConfig = {
 
 
 gulp.task('clear', function () {
-  return del('*', {cwd: 'dist'});
+  return deleteAsync(paths.output, {cwd: 'dist'});
+});
+gulp.task('clear_npm', function() {
+  return deleteAsync('/node_modules');
 });
 
-gulp.task('build', function () {
-  return gulp.src('knockout.mapping.js')
+gulp.task('build', function (done) {
+  gulp.src('knockout.mapping.js')
     .pipe(plumber({errorHandler: process.env.NODE_ENV === 'development'}))
     .pipe(jshint()).pipe(jshint.reporter('jshint-stylish'))
     .pipe(header(buildConfig.banner, {pkg: buildConfig.pkg}))
@@ -41,6 +51,11 @@ gulp.task('build', function () {
     .pipe(uglify())
     .pipe(sourceMaps.write('./'))
     .pipe(gulp.dest(buildConfig.outputPath));
+
+
+    gulp.src("./knockout.mapping.d.ts").pipe(gulp.dest("dist", { overwrite: true }));
+
+    return done();
 });
 
 gulp.task('jshint', function () {
